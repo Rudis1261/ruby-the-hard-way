44e32d48035048bd87e2e6f035095d103b36a2c8
Rudi Strydom <iam@thatguy.co.za>
2017-09-23 10:56:30 +0200

    Adding new excersizes

---
+++ Exercises/ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSept\nOct\nNov\nDec"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something on on here.
+With this weird quote
+We'll be able to type as much as we like
+Even 4 line if we want, or 5, or 6.
+}
