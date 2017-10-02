80ca36d494d59362431108ff2c39494c3e5a9d23
Rudi Strydom <iam@thatguy.co.za>
2017-10-02 22:05:55 +0200

    Updates

--- Exercises/ex17.rb
+++ Exercises/ex17.rb
@@ -1,14 +1,14 @@
 from_file, to_file = ARGV
 
+if (from_file == nil || to_file == nil)
+    puts "Missing required command line arguments. 'ruby ex17.rb FromFile ToFile'"
+    exit 1
+end
+
+puts "Copying from #{from_file} to #{to_file}"
+
 File.open(to_file, 'w') { |f| f.write(File.read(from_file)) }
 #open(to_file, 'w').write(open(from_file).read)
-
-# if (from_file == nil || to_file == nil)
-#     puts "Missing required command line arguments. 'ruby ex17.rb FromFile ToFile'"
-#     exit 1
-# end
-#
-# puts "Copying from #{from_file} to #{to_file}"
 #
 # # we could do these two on one line, how?
 # in_file = open(from_file)
